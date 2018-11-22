5f6661bc86c6404b1031cb86356573e2b318715b
Benjamin Sugden <benjisugden@bluewin.ch>
2018-11-22 14:03:40 +0100

    remove testing code and fix routes files.

--- example-app/app.go
+++ example-app/app.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"reflect"
 
 	"github.com/benschs/go-api/rest"
 	"github.com/go-chi/chi/middleware"
@@ -16,31 +13,7 @@ const (
 	ROUTES_FILE = "routes.yaml"
 )
 
-type Test struct {
-	Name string `json:"name"`
-}
-
 func main() {
-
-	jsonString := `{
-	"name": "test"
-}`
-
-	var body interface{}
-	e := json.Unmarshal([]byte(jsonString), &body)
-	if e != nil {
-		log.Fatal(e)
-	}
-
-	testBody := Test{Name: "test"}
-
-	v, e := rest.ParseType(testBody, reflect.TypeOf(Test{}))
-	if e != nil {
-		log.Fatal(e)
-	}
-
-	fmt.Println(v.(Test))
-
 	controller := createRestfulController()
 	controller.Use(middleware.Recoverer)
 
